700704b5ac256b96a6f3d3be41a84d5382d720a0
Sukhjeevan Singh <sukhi_ece@live.com>
2026-02-02 16:58:50 -0800

    Section 6 completed

--- client/src/features/activities/details/ActivityDetails.tsx
+++ client/src/features/activities/details/ActivityDetails.tsx
@@ -1,12 +1,16 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
-    activity: Activity
-    cancelSelectActivity: () => void
+    selectedActivity: Activity;
+    cancelSelect: () => void
     openForm: (id: string) => void
 }
 
-export default function ActivityDetail({ activity, cancelSelectActivity, openForm }: Props) {
+export default function ActivityDetail({ selectedActivity, cancelSelect, openForm }: Props) {
+    const { activities } = useActivities();
+    const activity = activities?.find(x => x.id === selectedActivity.id);
+    if (!activity) return <Typography>Loading...</Typography>
     return (
         <Card sx={{ borderRadius: 3 }}>
             <CardMedia
@@ -20,7 +24,7 @@ export default function ActivityDetail({ activity, cancelSelectActivity, openFor
             </CardContent>
             <CardActions>
                 <Button onClick={() => openForm(activity.id)} color="primary">Edit</Button>
-                <Button onClick={cancelSelectActivity} color="inherit">Cancel</Button>
+                <Button onClick={cancelSelect} color="inherit">Cancel</Button>
             </CardActions>
         </Card>
     )
